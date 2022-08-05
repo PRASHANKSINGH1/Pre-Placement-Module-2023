19187097d17b2680484a67bafd468ae7f3f78809
Prashank singh <75667356+PRASHANKSINGH1@users.noreply.github.com>
2022-08-05 23:31:40 +0530

    Create Move Zeroes.cpp

---
+++ Weekly Submissions/Week 2/Move Zeroes.cpp
@@ -0,0 +1,17 @@
+#include<iosteam>
+using namespace std
+int main()
+{
+        
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                nums[j++] = nums[i];
+            }
+        }
+        
+        for(int i = j; i < nums.size(); i++){
+            nums[i] = 0;
+        }
+        
+    }
